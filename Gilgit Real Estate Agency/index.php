88d700f458eda94326638d27fd6e836a6e490751
Mehmood Yar Baig <48985550+yara619@users.noreply.github.com>
2021-05-25 02:48:20 +0600

    Update index.php

--- Gilgit Real Estate Agency/index.php
+++ Gilgit Real Estate Agency/index.php
@@ -12,8 +12,8 @@ if(!$result){
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from thebootstrapthemes.com/live/thebootstrapthemes-realestate/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Apr 2017 02:43:16 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 <title>Gilgit Real Estate Agency</title>
 <meta charset="UTF-8" />
